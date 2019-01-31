7630bd0b0e5437e804a9e71d88f40109f784b9ab
visualstudio <visualstudio@230-PC42>
2019-01-31 14:42:33 -0500

    Commit at @@2:42

--- week4/Program.cs
+++ week4/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
-namespace week4
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    class Village
+    {
+        public static int numberofVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village() { Village.numberofVillages++; }
+    }
+    class Countryside
+    {
+        public Village Maple;
+
+        public Village Toronto;
+        public Village Ajax;
+
+        public void MapInitializer()
+        {
+            Maple = new Village();
+            Maple.VillageName = "Maple";
+            Maple.previousVillage = null;
+            Maple.nextVillage = Toronto;
+            Toronto = new Village();
+            Toronto.previousVillage = Maple;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Ajax = new Village();
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+
+
+        }
+        public void LookForAstrilde()
+        {
+
         }
     }
 }
